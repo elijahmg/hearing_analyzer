e8d7c6b0b411bbc11bf84800152caf4d1fff9599
Ilya <ilya.fakhrutdinov@trusteca.com>
2019-01-14 23:55:32 +0100

    added volume buttons, play and stop

--- app/src/main/java/com/degree/eliif/hearinganalyzer/PlayWave.kt
+++ app/src/main/java/com/degree/eliif/hearinganalyzer/PlayWave.kt
@@ -3,70 +3,92 @@ package com.degree.eliif.hearinganalyzer
 import android.media.AudioFormat
 import android.media.AudioManager
 import android.media.AudioTrack
+import android.os.Build
+import androidx.annotation.RequiresApi
 
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class PlayWave {
-    private val SAMPLE_RATE = 170000
-    private val mAudio: AudioTrack
-    private val buffsize = AudioTrack.getMinBufferSize(
-            SAMPLE_RATE,
-            AudioFormat.CHANNEL_OUT_MONO,
-            AudioFormat.ENCODING_PCM_16BIT
+  private val SAMPLE_RATE = 170000
+  private val mAudio: AudioTrack
+  private val buffsize = AudioTrack.getMinBufferSize(
+    SAMPLE_RATE,
+    AudioFormat.CHANNEL_OUT_MONO,
+    AudioFormat.ENCODING_PCM_16BIT
+  )
+
+  private var LOOP_LENGTH = 1
+
+  private var sampleCount: Int = 0
+
+  private var FREQUENCY: Int = 1
+
+  private var VOLUME: Float = 0F
+
+  init {
+    mAudio = AudioTrack(
+      AudioManager.STREAM_MUSIC,
+      SAMPLE_RATE,
+      AudioFormat.CHANNEL_OUT_MONO,
+      AudioFormat.ENCODING_PCM_16BIT,
+      buffsize,
+      AudioTrack.MODE_STATIC
     )
+    mAudio.setVolume(0.0001F)
+  }
 
-    private var LOOP_LENGTH = 1
+  /**
+   * Set up wave length
+   */
+  fun setWave(frequency: Int) {
+    FREQUENCY = frequency
+    sampleCount = SAMPLE_RATE / frequency
+    val samples = ShortArray(sampleCount)
+    print(samples)
+    val amp = 32767
+    val twopi = 8.0 * Math.atan(1.0)
+    var phase = 0.0
 
-    private var sampleCount: Int = 0
-
-    private var FREQUENCY: Int = 1
-
-    init {
-        mAudio = AudioTrack(
-                AudioManager.STREAM_MUSIC,
-                SAMPLE_RATE,
-                AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT,
-                buffsize,
-                AudioTrack.MODE_STATIC
-        )
+    for (i in 0 until sampleCount) {
+      samples[i] = (amp * Math.sin(phase)).toShort()
+      phase += twopi * frequency / SAMPLE_RATE
     }
 
-    /**
-     * Set up wave length
-     */
-    fun setWave (frequency: Int) {
-        FREQUENCY = frequency
-        sampleCount = SAMPLE_RATE / frequency
-        val samples = ShortArray(sampleCount)
-        print(samples)
-        val amp = 32767
-        val twopi = 8.0 * Math.atan(1.0)
-        var phase: Double = 0.0
+    mAudio.write(samples, 0, sampleCount)
+  }
 
-        for (i in 0 until sampleCount) {
-            samples[i] = (amp * Math.sin(phase)).toShort()
-            phase += twopi * frequency / SAMPLE_RATE
-        }
+  /**
+   * Set up loop length
+   */
+  fun setLoopLength(time: Int) {
+    LOOP_LENGTH = time * FREQUENCY
+  }
 
-        mAudio.write(samples, 0, sampleCount)
-    }
+  /**
+   * Set up volume
+   */
+  fun makeLouder(): Float {
+    VOLUME += 0.001F
+    if (VOLUME > 1 ) return 1.2F
+    mAudio.setVolume(VOLUME)
+    return VOLUME
+  }
 
-    /**
-     * Set up loop length
-     */
-    fun setLoopLength(time: Int) {
-        LOOP_LENGTH = time * FREQUENCY
-    }
+  fun makeQuiter(): Float {
+    VOLUME -= 0.001F
+    mAudio.setVolume(VOLUME)
+    return VOLUME
+  }
 
-    /** 1sec - 1 Hertz **/
-    fun play() {
-        mAudio.reloadStaticData()
-        mAudio.setLoopPoints(0, sampleCount, LOOP_LENGTH)
-        mAudio.play()
-    }
+  /** 1sec - 1 Hertz **/
+  fun play() {
+    mAudio.reloadStaticData()
+    mAudio.setLoopPoints(0, sampleCount, -1)
+    mAudio.play()
+  }
 
-    fun stop() {
-        mAudio.pause()
-        mAudio.flush()
-    }
+  fun stop() {
+    mAudio.pause()
+    mAudio.flush()
+  }
 
 }
